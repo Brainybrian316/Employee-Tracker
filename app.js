744f1608b7043379e16be103ba0dbec446c92f2b
Brainybrian316 <brainybrian316@gmail.com>
2022-05-08 12:52:46 -0400

    added ability to add employee

--- app.js
+++ app.js
@@ -64,4 +64,76 @@ function viewEmployeeByDepartment() {
     })
 }
 
+// function for adding employee
+function addEmployee() {
+    console.log('Adding Employee\n');
+
+    const query = `SELECT * FROM roles`;
+
+    db.query(query, function (err, res) {
+        if (err) { throw err; }
+        const roles = res.map(role => {
+            return {
+                name: role.title,
+                salary: role.salary,
+                value: role.id
+            }
+        })
+        console.table(res);
+        console.log('Roles Available\n');
+
+        promptChoices(roles);
+    })
+}
+
+// function for prompting choices to add employee
+function promptChoices(roles) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: "What is the employee's first name?"
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: "What is the employee's last name?"
+        },
+        {
+            type: 'list',
+            name: 'roleId',
+            message: "What is the employee's role?",
+            choices: roles
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: "What is the employee's manager ID?",
+        },
+        {
+            type: 'confirm',
+            name: 'is_manager',
+            message: "Is the employee a manager?",
+            default: false
+        },
+    ]).then(function (answers){
+        console.table(answers);
+        const query = `INSERT INTO employees (first_name, last_name, role_id, manager_id, is_manager)
+        VALUES (?, ?, ?, ?, ?)`;
+        db.query(query, [answers.first_name, answers.last_name, answers.roleId, answers.managerId, answers.is_manager], function (err, res) {
+            if (err) { throw err; }
+            
+            // console table response with \n
+            console.table(res)
+            // console table employee name added
+            console.log(`${answers.first_name} ${answers.last_name} added to database\n`);
+        
+
+            initialPrompt();
+        })
+    })
+};
+
+
+
 initialPrompt()
