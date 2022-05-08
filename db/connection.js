672926eca3a9033c50d861068d6c873c1e3a762a
Brainybrian316 <brainybrian316@gmail.com>
2022-05-08 11:43:52 -0400

    finished working with database

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,16 @@
+// initial modules
 const mysql = require('mysql2');
 require('dotenv').config();
 
 // connect to database
 const db = mysql.createConnection({
-    // connection 
+    // location of database
     host: 'localhost',
     // username
     user: process.env.DB_USER,
     // password
     password: process.env.DB_PASSWORD,
-    // database
+    // database name
     database: 'employee_data'
 
 },
